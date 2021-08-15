e8a7e71839a7bc510c28225808080907f0e6e26d
Sreejith Gopinathan <jithappan@gmail.com>
2021-08-15 13:39:18 +0530

    Initial commit of Basket microservice

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            this.basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            ShoppingCart basket = await basketRepository.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody]ShoppingCart basket)
+        {
+            return Ok(await basketRepository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> DeleteBasket(string userName)
+        {
+            await basketRepository.DeleteBasket(userName);
+            return Ok();
+        }
+    }// class ends
+}
---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Items.Sum(x => x.Quantity * x.Price);
+            }
+        }
+    }// class ends
+}
